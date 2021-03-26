498dcdc27d1d98c21a46bb46fde8e13601c3140c
DanielTuttos <tutorialesdanielromero@gmail.com>
2021-03-26 11:12:53 -0500

    Primer commit

---
+++ ExtraerTexto/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ExtraerTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExtraer = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fecha_hola = new System.Windows.Forms.DateTimePicker();
+            this.convertir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExtraer
+            // 
+            this.btnExtraer.Location = new System.Drawing.Point(83, 349);
+            this.btnExtraer.Name = "btnExtraer";
+            this.btnExtraer.Size = new System.Drawing.Size(204, 30);
+            this.btnExtraer.TabIndex = 0;
+            this.btnExtraer.Text = "Abrir Imagen";
+            this.btnExtraer.UseVisualStyleBackColor = true;
+            this.btnExtraer.Click += new System.EventHandler(this.btnExtraer_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(32, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(420, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 314);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Imagen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(578, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Texto Extraido";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fecha_hola
+            // 
+            this.fecha_hola.Location = new System.Drawing.Point(411, 378);
+            this.fecha_hola.Name = "fecha_hola";
+            this.fecha_hola.Size = new System.Drawing.Size(221, 20);
+            this.fecha_hola.TabIndex = 5;
+            // 
+            // convertir
+            // 
+            this.convertir.Location = new System.Drawing.Point(638, 378);
+            this.convertir.Name = "convertir";
+            this.convertir.Size = new System.Drawing.Size(75, 23);
+            this.convertir.TabIndex = 6;
+            this.convertir.Text = "convertir";
+            this.convertir.UseVisualStyleBackColor = true;
+            this.convertir.Click += new System.EventHandler(this.convertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 457);
+            this.Controls.Add(this.convertir);
+            this.Controls.Add(this.fecha_hola);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExtraer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExtraer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker fecha_hola;
+        private System.Windows.Forms.Button convertir;
+    }
+}
+
---
+++ ExtraerTexto/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tesseract;
+
+namespace ExtraerTexto
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExtraer_Click(object sender, EventArgs e)
+        {
+
+            
+            if ( openFileDialog1.ShowDialog() == DialogResult.OK){
+                String strfilename = openFileDialog1.FileName;
+
+                Bitmap imagen = new Bitmap(strfilename);
+                pictureBox1.Image = (System.Drawing.Image)imagen;
+                String nombreImg = openFileDialog1.SafeFileName;
+
+                var img = new Bitmap(openFileDialog1.FileName);
+                var ocr = new TesseractEngine(@"D:\Daniel\aplicaciones\c#\ExtraerTexto\ExtraerTexto\tessdata", "eng",EngineMode.Default);
+                var page = ocr.Process(img);
+                textBox1.Text = page.GetText();
+            }
+
+
+
+
+        }
+
+
+        private void convertir_Click(object sender, EventArgs e)
+        {
+            
+            DateTimeOffset dateOffsetValue = DateTimeOffset.Parse(fecha_hola.Value.Date.ToString());
+            MessageBox.Show("fecha long: " + dateOffsetValue.ToString("fffffff"));
+        }
+    }
+}
