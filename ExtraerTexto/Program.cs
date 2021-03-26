63bb4c80b1c6dcb459e90b578a9e9cc2ac213272
DanielTuttos <tutorialesdanielromero@gmail.com>
2021-03-26 11:41:26 -0500

    nueva version

--- ExtraerTexto/Program.cs
+++ ExtraerTexto/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Squirrel;
+using System.Threading.Tasks;
 
 namespace ExtraerTexto
 {
@@ -12,8 +14,12 @@ namespace ExtraerTexto
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/DanielTuttos/pruebaDistribucioncsharp-ejemplo"))
+            {
+                await mgr.Result.UpdateApp();
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
